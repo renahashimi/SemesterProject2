fbc5344f737130c0b343acc5fcf298437e081b4a
Rena Hashimi <renhas54143@stud.noroff.no>
2024-09-20 20:27:26 +0200

    Listings JS API + all Pages

---
+++ src/js/listeners/allPages.mjs
@@ -0,0 +1,11 @@
+import * as handlers from "../handlers/index.mjs";
+
+export function allPageListener() {
+  handlers.checkAuthStatus();
+  handlers.setupOverlayListeners();
+  handlers.updateUIBasedOnAuth();
+  handlers.highlightActiveLink()
+  handlers.loadFooter();
+  handlers.handlePageLoad();
+  handlers.setupMobileMenu();
+}
