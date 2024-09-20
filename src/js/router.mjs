9d6abc5e43d4851c7f75f08369292cf00e4c2edf
Rena Hashimi <renhas54143@stud.noroff.no>
2024-09-21 00:51:33 +0200

    Update router.mjs

--- src/js/router.mjs
+++ src/js/router.mjs
@@ -12,42 +12,33 @@ export default function router() {
     case "/index.html":
       handlers.renderHomeListings();
       break;
-
     case "/feed/profile/":
       handlers.renderMyProfile();
       break;
-
     case "/feed/profile/profiles/":
       handlers.renderProfiles();
       break;
-
     case "/feed/profile/register/":
     case "/feed/profile/register/index.html":
       handlers.registerFormListener();
       break;
-
     case "/feed/profile/edit/":
       handlers.setUpdateProfileListener();
       break;
-
     case "/feed/listings/":
       handlers.renderAllListings();
       handlers.searchListings();
       handlers.placeBidListener();
       break;
-
     case "/feed/listings/create/":
       handlers.setupCreateListingForm();
       break;
-
     case "/feed/listings/edit/":
       handlers.setUpdateListingListener();
       break;
-
     case "/feed/listings/singleitem/":
       handlers.renderSingleItem();
       break;
-
     default:
       break;
   }
